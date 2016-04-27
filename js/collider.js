86d28b4f4edc4a8087f6d455c7120c2b10dc1b0f
simplon-mars-2-kevin-l <kevin.lachaise@gmail.com>
2016-04-27 18:47:39 +0200

    Melange code + Ajout plateform

---
+++ js/collider.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent('collider', {
+  schema: {},
+
+  init: function() {
+    this.el.sceneEl.addBehavior(this);
+  },
+
+  update: function () {
+    var sceneEl = this.el.sceneEl;
+    var mesh = this.el.getObject3D('mesh');
+    var object3D = this.el.object3D
+    var originPoint = this.el.object3D.position.clone();
+    for (var vertexIndex = 0; vertexIndex < mesh.geometry.vertices.length; vertexIndex++) {
+      var localVertex = mesh.geometry.vertices[vertexIndex].clone();
+      var globalVertex = localVertex.applyMatrix4( object3D.matrix );
+      var directionVector = globalVertex.sub( object3D.position );
+
+      var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+      var collisionResults = ray.intersectObjects( sceneEl.object3D.children, true );
+
+    }
+  }
+});
